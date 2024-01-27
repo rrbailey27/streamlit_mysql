d3e07b3186dcf5f64f636bd498d87f7611339b48
rrbailey27 <96890632+rrbailey27@users.noreply.github.com>
2024-01-27 13:41:58 -0500

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -25,7 +25,7 @@ conn = st.connection('mysql', type='sql')
 #st_autorefresh(interval=5000, key="fetch_data")
 
 def fetch_data():
-    rawdata = conn.query('SELECT * from esp32_dht ORDER BY time_stamp DESC LIMIT 10;', ttl=1)
+    rawdata = conn.query('SELECT * from esp32_dht ORDER BY time_stamp DESC LIMIT 300;', ttl=1)
     df = pd.DataFrame(rawdata) #convert to transposed dataframe
     return df
 
