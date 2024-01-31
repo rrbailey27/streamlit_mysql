dc4a19b67b77e428b4066c252d77e41a52e98af9
rrbailey27 <96890632+rrbailey27@users.noreply.github.com>
2024-01-30 20:30:27 -0500

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -40,11 +40,9 @@ while True:
     old_temp, old_humidity = data.at[data.index[1], "tempF"], data.at[data.index[1], "humidity"]
     temp_delta, humid_delta = int(current_temp)-int(old_temp), int(current_humidity)-int(old_humidity)
 
-    st.text([data.at[data.index[0],"minute"]])    
-
-    
+   
     with display.container():
-        st.text([data.at[data.index[0],"ts"]])    
+        st.text([data.at[data.index[0],"minute"]])    
         
         # Create Summary Temperature Information
         lasttime, kpi1, kpi2 = st.columns(3)
