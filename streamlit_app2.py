deb97949aa7ea48284a8a3f1d129045d41326b6b
rrbailey27 <96890632+rrbailey27@users.noreply.github.com>
2024-01-30 22:46:12 -0500

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -24,7 +24,7 @@ conn = st.connection('mysql', type='sql')
 
 #st_autorefresh(interval=5000, key="fetch_data")
 
-@st.cache_data(ttl=1)
+@st.cache_data
 def fetch_data():
     rawdata = conn.query('SELECT tempF, humidity, YEAR(date_add(time_stamp,INTERVAL-5 HOUR)) as year, MONTH(date_add(time_stamp,INTERVAL-5 HOUR)) as month, DAY(date_add(time_stamp,INTERVAL-5 HOUR)) as day, HOUR(date_add(time_stamp,INTERVAL-5 HOUR)) as hour, MINUTE(date_add(time_stamp,INTERVAL-5 HOUR)) as minute, SECOND(date_add(time_stamp,INTERVAL-5 HOUR)) as second, date_add(time_stamp,INTERVAL-5 HOUR) as ts FROM esp32_dht ORDER BY time_stamp DESC LIMIT 4800;', ttl=1)
     #rawdata = conn.query('SELECT tempF, humidity, date_add(time_stamp,INTERVAL-5 HOUR) as ts FROM esp32_dht ORDER BY time_stamp DESC LIMIT 4800;')
